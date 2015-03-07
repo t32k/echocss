2c981318e6f792608e2eafeed4ad43d118c40b29
t32k <ijok.ijok@gmail.com>
2015-03-06 20:39:53 -0800

    3.2.0

--- railbook/db/schema.rb
+++ railbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307014225) do
+ActiveRecord::Schema.define(version: 20150307040713) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
