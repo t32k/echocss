4018309e2036ddcc17e73ea96270dee210c6e05f
t32k <ijok.ijok@gmail.com>
2015-03-06 16:31:39 -0800

    2.3.1

---
+++ railbook/app/controllers/hello_controller.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+class HelloController < ApplicationController
+
+  def index
+    @msg = 'こんちわ、世界！'
+  end
+
+end
--- railbook/config/routes.rb
+++ railbook/config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  match ':controller(/:action(/:id))', via: [ :get, :post, :patch ]
 end
